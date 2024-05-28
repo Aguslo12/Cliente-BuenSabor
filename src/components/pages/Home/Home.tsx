41a162f840d6451d799daa8bc339e6b6721e8d30
Agustin Lobos <aguslobos.1712@gmail.com>
2024-05-28 19:41:54 -0300

    Changes

--- src/components/pages/Home/Home.tsx
+++ src/components/pages/Home/Home.tsx
@@ -3,7 +3,7 @@ import { EmpresaInicio } from "../../ui/Empresa/EmpresaInicio"
 
 export const Home = () => {
   return (
-    <div className="h-screen bg-slate-600 items-center flex align-middle justify-center">
+    <div className="h-screen bg-backColor items-center flex align-middle justify-center">
         <EmpresaInicio/>
     </div>
   )
